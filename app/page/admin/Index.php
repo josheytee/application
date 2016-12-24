bec9e6c2c9688f37bb574ca4ee3d3e7967d22547
josheytee <t_josh24@hotmail.com>
2016-12-24 02:29:23 +0100

    admin and front folders and index files respeectively added

---
+++ app/page/admin/Index.php
@@ -0,0 +1,12 @@
+<?php
+
+use app\core\Page;
+
+/**
+ * Description of Index
+ *
+ * @author Tobi
+ */
+class Index extends Page {
+
+}
---
+++ app/page/front/Index.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Description of Index
+ *
+ * @author Tobi
+ */
+class Index {
+    //put your code here
+}
